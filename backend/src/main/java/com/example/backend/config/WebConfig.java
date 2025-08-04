e46330db8a7147be3cb129910c99caa5d11eb723
rladbflaz <youro1228@naver.com>
2025-08-04 16:27:30 +0900

    fix: 운영 환경에서 CORS 허용을 위해 S3 도메인 추가

--- backend/src/main/java/com/example/backend/config/WebConfig.java
+++ backend/src/main/java/com/example/backend/config/WebConfig.java
@@ -9,7 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173",
+                        "http://fullstack-frontend-app-bucket-yoo.s3-website-ap-southeast-2.amazonaws.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
